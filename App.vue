0699b21aa251048bff5787a4e2749b713f430902
Mohamed_Elhadidy <ilvolpo2022@gmail.com>
2021-12-16 06:20:00 +0200

    landing page data

---
+++ App.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="container">
+<NavBar/>
+<DevelopStore />
+<AllInOnePlace />
+<SpecialApps />
+  <router-view/>
+  </div>
+</template>
+
+<script>
+import NavBar from './views/NavBar.vue'
+import DevelopStore from './views/DevelopStore.vue'
+import AllInOnePlace from './views/AllInOnePlace.vue'
+import SpecialApps from './views/SpecialApps.vue'
+export default {
+  name:'App',
+  components:{
+    NavBar,
+    DevelopStore,
+    AllInOnePlace,
+    SpecialApps,
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ views/DevelopStore.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <h1> طور متجرك ووسع آفاق تجارتك مع سوق تطبيقات زد</h1>
+    <p>طور تجارتك من خلال تطبيقات و حلول سوق زد التى تساعدك فى نمو أرباحك عبر إدارة عمليات متجرك بشكل فعال و احترافى</p>
+
+      <button>تصفح التطبيق</button>
+<div>
+          <figure>
+<img src="../assets/Intro.png" alt="">
+        </figure>
+        <figure>
+<img src="../assets/yellow-ellipse.png" alt="">
+        </figure>
+        <figure>
+<img src="../assets/Dot-Pattern.png" alt="">
+        </figure>
+
+</div>
+</div>
+</template>
+<script>
+export default {
+  name:'DevelopStore',
+}
+</script>
+<style scoped>
+
+</style>
